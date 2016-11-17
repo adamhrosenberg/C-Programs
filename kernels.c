c7e1b8f3b6a9a8da16b3444bd7b8225ee414e7b8
Adam Rosenberg <adam.arosenberg@gmail.com>
2016-11-17 10:44:36 -0700

    Pushing 4400

---
+++ kernels.c
@@ -0,0 +1,385 @@
+/*******************************************
+ * Solutions for the CS:APP Performance Lab
+ ********************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "defs.h"
+
+//Adam Rosenberg
+//This code is to be used in a framework ready for it. Here, the objective is
+// to unroll complex loops and take advantage of temporal and spatial locality.
+
+/***************
+ * ROTATE KERNEL
+ ***************/
+
+/******************************************************
+ * Your different versions of the rotate kernel go here
+ ******************************************************/
+
+/* 
+ * naive_pinwheel - The naive baseline version of pinwheel 
+ */
+char naive_pinwheel_descr[] = "naive_pinwheel: Naive baseline implementation";
+void naive_pinwheel(int dim, pixel *src, pixel *dest)
+{
+  int i, j;
+
+  for (i = 0; i < dim/2; i++)
+    for (j = 0; j < dim/2; j++)
+      dest[RIDX(dim/2 - 1 - j, i, dim)] = src[RIDX(i, j, dim)];
+    
+  for (i = 0; i < dim/2; i++)
+    for (j = 0; j < dim/2; j++)
+      dest[RIDX(dim/2 - 1 - j, dim/2 + i, dim)] = src[RIDX(i, dim/2 + j, dim)];
+
+  for (i = 0; i < dim/2; i++)
+    for (j = 0; j < dim/2; j++)
+      dest[RIDX(dim - 1 - j, i, dim)] = src[RIDX(dim/2 + i, j, dim)];
+
+  for (i = 0; i < dim/2; i++)
+    for (j = 0; j < dim/2; j++)
+      dest[RIDX(dim - 1 - j, dim/2 + i, dim)] = src[RIDX(dim/2 + i, dim/2 + j, dim)];
+}
+
+/* 
+ * MINE!!!! BELOW
+ * rotate - Your current working version of pinwheel
+ * IMPORTANT: This is the version you will be graded on
+ */
+char pinwheel_descr[] = "pinwheel: Current working version";
+void pinwheel(int dim, pixel *src, pixel *dest)
+{
+  int i, j;
+  int halfdim = dim>>1;
+  int firstindex;
+
+
+//////first
+  for (i = 0; i < halfdim; i+=16)
+    for (j = 0; j < halfdim; j++){
+      firstindex = halfdim - 1 - j;
+      dest[RIDX(firstindex, i, dim)] = src[RIDX(i, j, dim)];
+      dest[RIDX(firstindex, i+1, dim)] = src[RIDX(i+1, j, dim)];
+      dest[RIDX(firstindex, i+2, dim)] = src[RIDX(i+2, j, dim)];
+      dest[RIDX(firstindex, i+3, dim)] = src[RIDX(i+3, j, dim)];
+      dest[RIDX(firstindex, i+4, dim)] = src[RIDX(i+4, j, dim)];
+      dest[RIDX(firstindex, i+5, dim)] = src[RIDX(i+5, j, dim)];
+      dest[RIDX(firstindex, i+6, dim)] = src[RIDX(i+6, j, dim)];
+      dest[RIDX(firstindex, i+7, dim)] = src[RIDX(i+7, j, dim)];
+      dest[RIDX(firstindex, i+8, dim)] = src[RIDX(i+8, j, dim)];
+      dest[RIDX(firstindex, i+9, dim)] = src[RIDX(i+9, j, dim)];
+      dest[RIDX(firstindex, i+10, dim)] = src[RIDX(i+10, j, dim)];
+      dest[RIDX(firstindex, i+11, dim)] = src[RIDX(i+11, j, dim)];
+      dest[RIDX(firstindex, i+12, dim)] = src[RIDX(i+12, j, dim)];
+      dest[RIDX(firstindex, i+13, dim)] = src[RIDX(i+13, j, dim)];
+      dest[RIDX(firstindex, i+14, dim)] = src[RIDX(i+14, j, dim)];
+      dest[RIDX(firstindex, i+15, dim)] = src[RIDX(i+15, j, dim)];
+  }
+
+
+///////second
+  for (i = 0; i < halfdim; i+=16)
+    for (j = 0; j < halfdim; j++){
+      //dest[RIDX(dim/2 - 1 - j, dim/2 + i, dim)] = src[RIDX(i, dim/2 + j, dim)];
+      dest[RIDX(halfdim - 1 - j, halfdim + i, dim)] = src[RIDX(i, halfdim + j, dim)];
+      dest[RIDX(halfdim - 1 - j, halfdim + i+1, dim)] = src[RIDX(i+1, halfdim + j, dim)];
+      dest[RIDX(halfdim - 1 - j, halfdim + i+2, dim)] = src[RIDX(i+2, halfdim + j, dim)];
+      dest[RIDX(halfdim - 1 - j, halfdim + i+3, dim)] = src[RIDX(i+3, halfdim + j, dim)];
+      dest[RIDX(halfdim - 1 - j, halfdim + i+4, dim)] = src[RIDX(i+4, halfdim + j, dim)];
+      dest[RIDX(halfdim - 1 - j, halfdim + i+5, dim)] = src[RIDX(i+5, halfdim + j, dim)];
+      dest[RIDX(halfdim - 1 - j, halfdim + i+6, dim)] = src[RIDX(i+6, halfdim + j, dim)];
+      dest[RIDX(halfdim - 1 - j, halfdim + i+7, dim)] = src[RIDX(i+7, halfdim + j, dim)];
+      dest[RIDX(halfdim - 1 - j, halfdim + i+8, dim)] = src[RIDX(i+8, halfdim + j, dim)];
+      dest[RIDX(halfdim - 1 - j, halfdim + i+9, dim)] = src[RIDX(i+9, halfdim + j, dim)];
+      dest[RIDX(halfdim - 1 - j, halfdim + i+10, dim)] = src[RIDX(i+10, halfdim + j, dim)];
+      dest[RIDX(halfdim - 1 - j, halfdim + i+11, dim)] = src[RIDX(i+11, halfdim + j, dim)];
+      dest[RIDX(halfdim - 1 - j, halfdim + i+12, dim)] = src[RIDX(i+12, halfdim + j, dim)];
+      dest[RIDX(halfdim - 1 - j, halfdim + i+13, dim)] = src[RIDX(i+13, halfdim + j, dim)];
+      dest[RIDX(halfdim - 1 - j, halfdim + i+14, dim)] = src[RIDX(i+14, halfdim + j, dim)];
+      dest[RIDX(halfdim - 1 - j, halfdim + i+15, dim)] = src[RIDX(i+15, halfdim + j, dim)];
+   }
+
+
+
+////////////third
+  for (i = 0; i < halfdim; i+=16)
+    for (j = 0; j < halfdim; j++){
+      //dest[RIDX(dim - 1 - j, i, dim)] = src[RIDX(dim/2 + i, j, dim)];
+      dest[RIDX(dim - 1 - j, i, dim)] = src[RIDX(halfdim + i, j, dim)];
+      dest[RIDX(dim - 1 - j, i+1, dim)] = src[RIDX(halfdim + i+1, j, dim)];
+      dest[RIDX(dim - 1 - j, i+2, dim)] = src[RIDX(halfdim + i+2, j, dim)];
+      dest[RIDX(dim - 1 - j, i+3, dim)] = src[RIDX(halfdim + i+3, j, dim)];
+      dest[RIDX(dim - 1 - j, i+4, dim)] = src[RIDX(halfdim + i+4, j, dim)];
+      dest[RIDX(dim - 1 - j, i+5, dim)] = src[RIDX(halfdim + i+5, j, dim)];
+      dest[RIDX(dim - 1 - j, i+6, dim)] = src[RIDX(halfdim + i+6, j, dim)];
+      dest[RIDX(dim - 1 - j, i+7, dim)] = src[RIDX(halfdim + i+7, j, dim)];
+      dest[RIDX(dim - 1 - j, i+8, dim)] = src[RIDX(halfdim + i+8, j, dim)];
+      dest[RIDX(dim - 1 - j, i+9, dim)] = src[RIDX(halfdim + i+9, j, dim)];
+      dest[RIDX(dim - 1 - j, i+10, dim)] = src[RIDX(halfdim + i+10, j, dim)];
+      dest[RIDX(dim - 1 - j, i+11, dim)] = src[RIDX(halfdim + i+11, j, dim)];
+      dest[RIDX(dim - 1 - j, i+12, dim)] = src[RIDX(halfdim + i+12, j, dim)];
+      dest[RIDX(dim - 1 - j, i+13, dim)] = src[RIDX(halfdim + i+13, j, dim)];
+      dest[RIDX(dim - 1 - j, i+14, dim)] = src[RIDX(halfdim + i+14, j, dim)];
+      dest[RIDX(dim - 1 - j, i+15, dim)] = src[RIDX(halfdim + i+15, j, dim)];
+    }
+
+/////fourth
+  for (i = 0; i < halfdim; i+=16)
+    for (j = 0; j < halfdim; j++){
+      //dest[RIDX(dim - 1 - j, dim/2 + i, dim)] = src[RIDX(dim/2 + i, dim/2 + j, dim)];
+      dest[RIDX(dim - 1 - j, halfdim + i, dim)] = src[RIDX(halfdim + i, halfdim + j, dim)];
+      dest[RIDX(dim - 1 - j, halfdim + i+1, dim)] = src[RIDX(halfdim + i+1, halfdim + j, dim)];
+      dest[RIDX(dim - 1 - j, halfdim + i+2, dim)] = src[RIDX(halfdim + i+2, halfdim + j, dim)];
+      dest[RIDX(dim - 1 - j, halfdim + i+3, dim)] = src[RIDX(halfdim + i+3, halfdim + j, dim)];
+      dest[RIDX(dim - 1 - j, halfdim + i+4, dim)] = src[RIDX(halfdim + i+4, halfdim + j, dim)];
+      dest[RIDX(dim - 1 - j, halfdim + i+5, dim)] = src[RIDX(halfdim + i+5, halfdim + j, dim)];
+      dest[RIDX(dim - 1 - j, halfdim + i+6, dim)] = src[RIDX(halfdim + i+6, halfdim + j, dim)];
+      dest[RIDX(dim - 1 - j, halfdim + i+7, dim)] = src[RIDX(halfdim + i+7, halfdim + j, dim)];
+      dest[RIDX(dim - 1 - j, halfdim + i+8, dim)] = src[RIDX(halfdim + i+8, halfdim + j, dim)];
+      dest[RIDX(dim - 1 - j, halfdim + i+9, dim)] = src[RIDX(halfdim + i+9, halfdim + j, dim)];
+      dest[RIDX(dim - 1 - j, halfdim + i+10, dim)] = src[RIDX(halfdim + i+10, halfdim + j, dim)];
+      dest[RIDX(dim - 1 - j, halfdim + i+11, dim)] = src[RIDX(halfdim + i+11, halfdim + j, dim)];
+      dest[RIDX(dim - 1 - j, halfdim + i+12, dim)] = src[RIDX(halfdim + i+12, halfdim + j, dim)];
+      dest[RIDX(dim - 1 - j, halfdim + i+13, dim)] = src[RIDX(halfdim + i+13, halfdim + j, dim)];
+      dest[RIDX(dim - 1 - j, halfdim + i+14, dim)] = src[RIDX(halfdim + i+14, halfdim + j, dim)];
+      dest[RIDX(dim - 1 - j, halfdim + i+15, dim)] = src[RIDX(halfdim + i+15, halfdim + j, dim)];
+    }
+}
+
+/*********************************************************************
+ * register_pinwheel_functions - Register all of your different versions
+ *     of the pinwheel kernel with the driver by calling the
+ *     add_pinwheel_function() for each test function. When you run the
+ *     driver program, it will test and report the performance of each
+ *     registered test function.  
+ *********************************************************************/
+
+void register_pinwheel_functions() {
+  add_pinwheel_function(&pinwheel, pinwheel_descr);
+  add_pinwheel_function(&naive_pinwheel, naive_pinwheel_descr);
+}
+
+
+/***************
+ * SMOOTH KERNEL
+ **************/
+
+/***************************************************************
+ * Various typedefs and helper functions for the smooth function
+ * You may modify these any way you like.
+ **************************************************************/
+
+/* A struct used to compute averaged pixel value */
+typedef struct {
+  int red;
+  int green;
+  int blue;
+} pixel_sum;
+
+/* 
+ * initialize_pixel_sum - Initializes all fields of sum to 0 
+ */
+static void initialize_pixel_sum(pixel_sum *sum) 
+{
+  sum->red = sum->green = sum->blue = 0;
+  return;
+}
+
+/* 
+ * accumulate_sum - Accumulates field values of p in corresponding 
+ * fields of sum 
+ */
+static void accumulate_weighted_sum(pixel_sum *sum, pixel p, double weight) 
+{
+  sum->red += (int) p.red * weight;
+  sum->green += (int) p.green * weight;
+  sum->blue += (int) p.blue * weight;
+  return;
+}
+
+/* 
+ * assign_sum_to_pixel - Computes averaged pixel value in current_pixel 
+ */
+static void assign_sum_to_pixel(pixel *current_pixel, pixel_sum sum) 
+{
+  current_pixel->red = (unsigned short)sum.red;
+  current_pixel->green = (unsigned short)sum.green;
+  current_pixel->blue = (unsigned short)sum.blue;
+  return;
+}
+
+/* 
+ * weighted_combo - Returns new pixel value at (i,j) 
+ */
+static pixel weighted_combo(int dim, int i, int j, pixel *src) 
+{
+  int ii, jj;
+  pixel_sum sum;
+  pixel current_pixel;
+  double weights[3][3] = { { 0.50, 0.03125, 0.00 },
+                           { 0.03125, 0.25, 0.03125 },
+                           { 0.00, 0.03125, 0.125 } };
+
+  initialize_pixel_sum(&sum);
+  for(ii=0; ii < 3; ii++)
+    for(jj=0; jj < 3; jj++) 
+      if ((i + ii < dim) && (j + jj < dim))
+        accumulate_weighted_sum(&sum,
+                                src[RIDX(i+ii,j+jj,dim)],
+                                weights[ii][jj]);
+    
+  assign_sum_to_pixel(&current_pixel, sum);
+ 
+  return current_pixel;
+}
+
+/******************************************************
+ * Your different versions of the motion kernel go here
+ ******************************************************/
+
+/*
+ * naive_motion - The naive baseline version of motion 
+ */
+char naive_motion_descr[] = "naive_motion: Naive baseline implementation";
+void naive_motion(int dim, pixel *src, pixel *dst) 
+{
+  int i, j;
+    
+  for (i = 0; i < dim; i++)
+    for (j = 0; j < dim; j++)
+      dst[RIDX(i, j, dim)] = weighted_combo(dim, i, j, src);
+}
+
+
+
+
+/**********************************
+ * MINE!!!! BELOW!!!!!
+ * motion - Your current working version of motion. 
+ * IMPORTANT: This is the version you will be graded on
+ */
+char motion_descr[] = "motion: Current working version";
+void motion(int dim, pixel *src, pixel *dst) 
+{
+  int i, j;
+
+//HELPER FUNC DECS
+  int ii, jj;
+  pixel_sum sum;
+  pixel current_pixel;
+
+ 
+  for (i = 0; i < dim; i++){
+    for (j = 0; j < dim; j++){
+      //helper..
+       //initialize_pixel_sum(&sum);
+       	sum.red = 0;
+	sum.green = 0;
+	sum.blue = 0;
+
+      
+	ii = 0;
+     	jj = 0;
+
+	//double weight;
+	pixel p;
+ 	p = src[RIDX(i+ii,j+jj,dim)];   
+           if ((i + ii < dim) && (j + jj < dim)){
+              	//p = src[RIDX(i+ii,j+jj,dim)];                
+		sum.red += (int) p.red >>1;
+  		sum.green += (int) p.green >>1;
+  		sum.blue += (int) p.blue >>1;
+           }
+//0, 1
+     	jj++;
+ 	p = src[RIDX(i+ii,j+jj,dim)];   
+
+           if ((i + ii < dim) && (j + jj < dim)){
+              	//p = src[RIDX(i+ii,j+jj,dim)];                
+		sum.red += (int) p.red >> 5;
+  		sum.green += (int) p.green >> 5;
+  		sum.blue += (int) p.blue >> 5;
+           }
+//0,2
+
+	ii = 1;
+     	jj = 0;
+ 	p = src[RIDX(i+ii,j+jj,dim)];   
+           if ((i + ii < dim) && (j + jj < dim)){
+              //	p = src[RIDX(i+ii,j+jj,dim)];                
+		sum.red += (int) p.red >> 5;
+  		sum.green += (int) p.green >> 5;
+  		sum.blue += (int) p.blue >> 5;
+
+           }
+     	jj++;
+//1,1
+	p = src[RIDX(i+ii,j+jj,dim)];   
+           if ((i + ii < dim) && (j + jj < dim)){
+                               
+		sum.red += (int) p.red >>2;
+  		sum.green += (int) p.green >> 2;
+  		sum.blue += (int) p.blue >> 2;
+
+           }
+     	jj++;
+//1,2
+	p = src[RIDX(i+ii,j+jj,dim)];   
+
+           if ((i + ii < dim) && (j + jj < dim)){
+
+              //	p = src[RIDX(i+ii,j+jj,dim)];                
+		sum.red += (int) p.red >> 5;
+  		sum.green += (int) p.green >>5;
+  		sum.blue += (int) p.blue >>5;
+
+           }
+	ii = 2;
+     	jj = 1;
+
+//2,1
+	p = src[RIDX(i+ii,j+jj,dim)];   
+           if ((i + ii < dim) && (j + jj < dim)){        
+		sum.red += (int) p.red >> 5;
+  		sum.green += (int) p.green >> 5;
+  		sum.blue += (int) p.blue >> 5;
+
+           }
+     	jj++;
+//2,2
+	p = src[RIDX(i+ii,j+jj,dim)];   
+           if ((i + ii < dim) && (j + jj < dim)){
+               
+                          
+		sum.red += (int) p.red >> 3;
+  		sum.green += (int) p.green >> 3;
+  		sum.blue += (int) p.blue >>3;
+           }
+
+		current_pixel.red = (unsigned short)sum.red;
+ 		current_pixel.green = (unsigned short)sum.green;
+    		current_pixel.blue = (unsigned short)sum.blue;
+
+
+           dst[RIDX(i, j, dim)] = current_pixel;
+
+
+       }
+    }
+  
+}
+
+/********************************************************************* 
+ * register_motion_functions - Register all of your different versions
+ *     of the motion kernel with the driver by calling the
+ *     add_motion_function() for each test function.  When you run the
+ *     driver program, it will test and report the performance of each
+ *     registered test function.  
+ *********************************************************************/
+
+void register_motion_functions() {
+  add_motion_function(&motion, motion_descr);
+  add_motion_function(&naive_motion, naive_motion_descr);
+}
---
+++ matchlab.c
@@ -0,0 +1,320 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// adam rosenberg
+// Objective here it so recognize regular expressions as assigned in the lab
+
+int aflag = 0;
+int bflag = 0;
+int cflag = 0;
+int tflag = 0;
+
+
+void doaflag(char buffer[]){
+
+/**
+ * a flag
+ *
+ * between 4 and 5 repetitions (inclusive) of the letter “k”;
+3 or more repetitions of the letter “v”; and
+an odd number of uppercase letters.
+ */
+
+    const int length = strlen(buffer);
+    int kcount = 0; //k = 107
+    int vcount = 0;  //v = 188
+    int uppercount = 0; //capitol is 65-90
+    int othercount = 0;
+    int order = 1;
+    int i;
+    for(i = 0; i < length; i ++){
+        if(buffer[i] == 107){
+            kcount++;
+        }else if(buffer[i] == 118){
+            vcount++;
+        }else if(buffer[i] <= 90 && buffer[i] >=65){
+            uppercount++;
+        }else{
+            othercount++;
+        }
+    }
+
+    if(buffer[0] != 'k')
+        order = 0;
+    if(buffer[4] == 'k' && !(buffer[5] == 'v'))
+        order = 0;
+    if(buffer[3] == 'k' && !(buffer[4] == 'v'))
+       order = 0;
+    if(!(buffer[vcount+kcount] >= 'A' && buffer[vcount+kcount] <= 'Z'))
+        order = 0;
+
+
+    if((kcount == 4 || kcount == 5) && vcount >=3 && ((uppercount&1)==1) && othercount == 0 && order){
+
+        if(tflag){
+            char first = buffer[0];
+            for(i = 0; i < length; i++){
+                buffer[i] = first;
+                //if t flag...print.
+                printf("%c", buffer[i]);
+            }
+            printf("\n");
+        }else{
+            printf("yes\n");
+        }
+    }
+    else{
+
+        if(!tflag){
+            printf("no\n");
+        }
+    }
+}
+void dobflag(char buffer[]){
+
+    /**
+     *  any even number (including zero) repetitions of the letter “h”;
+        between 1 and 3 (inclusive) decimal digits — call this sequence X;
+        between 3 and 6 repetitions (inclusive) of the letter “n”;
+        the same character sequence as X, but reversed; and
+        an odd number of uppercase letters.
+            For matches, perform the following conversion:
+            add one “E” before each “G”.
+     */
+    const int length = strlen(buffer);
+    int order = 1;
+    int hcount = 0; //h = 104
+    int deccount = 0; // 48-57 is a num 0-9
+    int numbers[6];
+    int ncount = 0; //n = 110
+    int isdecreversed = 0; //0 false 1 true
+    int uppercount = 0; //upper is 65-90
+    int othercount=0;
+
+    int i;
+    for (i = 0; i < length; i++) {
+        if (buffer[i] == 104) {
+            hcount++;
+        } else if (buffer[i] == 110) {
+            ncount++;
+        } else if (buffer[i] <= 90 && buffer[i] >= 65) {
+            uppercount++;
+        } else if (buffer[i] >= 48 && buffer[i] <= 57) {
+
+            if(deccount < 6){
+                numbers[deccount] = buffer[i];
+            }
+            deccount++;
+            //add constraint..maybe? lol.
+        }else{
+            othercount++;
+        }
+    }
+
+    if(hcount == 0 && !(buffer[0]>= 48 && buffer[0] <= 57 )){
+        order = 0;
+    }
+    if(!(buffer[length-1] >= 'A' && buffer[length-1] <= 'Z'))
+        order = 0;
+
+
+    if (deccount == 2) {
+        if(buffer[hcount+1]!='n')
+            order = 0;
+        if (numbers[0] == numbers[1])
+            isdecreversed = 1;
+    } else if (deccount == 4) {
+        if(buffer[hcount+2] != 'n')
+            order = 0;
+        if (numbers[0] == numbers[3] && numbers[1] == numbers[2])
+            isdecreversed = 1;
+    } else if (deccount == 6) {
+        if(buffer[hcount + 3] != 'n')
+            order = 0;
+        if (numbers[0] == numbers[5] && numbers[1] == numbers[4] && numbers[2] == numbers[3])
+            isdecreversed = 1;
+    } else {
+        isdecreversed = 0;
+    }
+
+//    printf("is reversed %d, h count %d, n count %d, uppercount %d, othercount %d, deccount %d, order %d \n", isdecreversed, hcount, ncount, uppercount, othercount, deccount, order);
+
+    if((isdecreversed == 1) && (hcount%2==0 || hcount == 0) && (ncount > 2 && ncount < 7) && (uppercount%2 == 1) && othercount==0 && order ){
+        int extra = 0;
+
+        if(tflag){
+            for(i = 0 ; i < length+extra; i++){
+                if(buffer[i] == 71){
+                    printf("E");
+                }
+                printf("%c", buffer[i]);
+            }
+            printf("\n");
+        }else{
+            printf("yes\n");
+        }
+    }else{
+        if(!tflag){
+            printf("no\n");
+        }
+    }
+}
+void docflag(char buffer[]){
+
+    /**
+     *  any odd number of repetitions of the letter “a”;
+        between 1 and 3 (inclusive) decimal digits — call this sequence X;
+        any odd number of repetitions of the letter “y”;
+        an odd number of uppercase letters; and
+        the same characters as the odd-positioned characters in X.
+
+        For matches, perform the following conversion:
+        remove every “C” and “G”.
+     */
+    const int length = strlen(buffer);
+    int acount = 0; // 97
+    int deccount = 0; //48-57
+    int ycount = 0; //121
+    int uppercount = 0; //65-90
+    int numbers[5];
+    int othercount = 0;
+    int isdecodd=0;
+    int startsatodd = 0;
+    int order = 1;
+
+    int i;
+    for (i = 0; i < length; i++) {
+        if (buffer[i] == 97) {
+            acount++;
+        } else if (buffer[i] == 121) {
+            ycount++;
+        } else if (buffer[i] <= 90 && buffer[i] >= 65) {
+            uppercount++;
+        } else if (buffer[i] >= 48 && buffer[i] <= 57) {
+            if(deccount < 5){
+                numbers[deccount] = buffer[i];
+//                printf("deccount = %d, numbers[deccount] = %d, buffer[%d] = %d\n", deccount, numbers[deccount], i, buffer[i]);
+                deccount++;
+            }
+        }else{
+            othercount++;
+        }
+    }
+
+
+    /**
+*  any odd number of repetitions of the letter “a”;
+   between 1 and 3 (inclusive) decimal digits — call this sequence X;
+   any odd number of repetitions of the letter “y”;
+   an odd number of uppercase letters; and
+   the same characters as the odd-positioned characters in X.
+
+   For matches, perform the following conversion:
+   remove every “C” and “G”.
+*/
+
+    if(buffer[0] != 'a'){
+        order = 0;
+    }
+    if(!(buffer[acount] >= 48 && buffer[acount] <= 57))
+        order = 0;
+
+    if(!(buffer[length-1] >= 48 && buffer[length-1] <= 57))
+        order = 0;
+
+
+    for(i = 0 ; i < 5; i++) {
+        if (deccount == 2) { //len 1
+            isdecodd = 0;
+        } else if (deccount == 3) { //len 2
+            if (numbers[1] == numbers[2]){
+                isdecodd = 1;
+            }
+        } else if (deccount == 4) { //len 3
+            if (numbers[1] == numbers[3])
+                isdecodd = 1;
+        } else {
+            isdecodd = 0;
+        }
+
+    }
+
+
+//        printf("is odd %d, a count %d, y count %d, uppercount %d, othercount %d, deccount %d \n", isdecodd, acount,
+//               ycount, uppercount, othercount, deccount);
+    if ((acount & 1) == 1 && isdecodd == 1 && ((ycount & 1) == 1) && (uppercount & 1) == 1 && othercount == 0 && order) {
+        if(tflag){
+            for(i =0; i < length; i++){
+                if(buffer[i] != 'C' && buffer[i] != 'G')
+                    printf("%c", buffer[i]);
+            }
+            printf("\n");
+        }
+        else{
+            printf("yes\n");
+        }
+        //adapt this for odd positions of x...
+    } else {
+        if(tflag){
+
+        }else{
+            printf("no\n");
+        }
+    }
+
+}
+
+
+//driver method to call the correct methods for each string. (char array)
+void dowork(char buffer[]){
+    const int length = strlen(buffer);
+
+    if(aflag)
+        doaflag(buffer);
+    else if(bflag)
+        dobflag(buffer);
+    else if(cflag)
+        docflag(buffer);
+    else{
+        doaflag(buffer);
+    }
+}
+
+
+//main method. parses for flags, then passes each 'string' to a driver method that calls the right method based on active flags.
+int main (int argc, char *argv[])
+{
+    int count = 0;
+
+    char buffer[100]; //malloc
+    int i;
+    for(i = 0; i < 100; i ++){
+        buffer[i]=' ';
+    }
+
+//    //searching for a dash and finding the flags after that..
+    for(i = 1; i < argc; i++){
+        if(argv[i][0] == '-'){ //str[0] = argv...
+            char argument = argv[i][1];
+                switch (argument) {
+                    case 'a':
+                        aflag = 1;
+                        break;
+                    case 'b':
+                        bflag = 1;
+                        break;
+                    case 'c':
+                        cflag = 1;
+                        break;
+                    case 't':
+                        tflag = 1;
+                        break;
+                }
+        }else{
+            dowork(argv[i]); //pass each element of buffer call alg here
+        }
+    }
+    return 0;
+}
+
